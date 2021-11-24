0517c9befc6b5be955bd29f8f79569d5f6dcda8f
amazonever18 <amazonever18@gmail.com>
2021-11-25 00:17:55 +0900

    'Commit'

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from threading import Thread
 import schedule
 from app.controllers.webserver import start
 from yahoo import yahoo_finance
+from app.controllers.webserver import app as application
+
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 if __name__ == "__main__":
+    # Yahoo Finance からデータを取得し、DBに格納
+    # yahoo_finance.generate_all_values()
+
     # サーバを起動
     # serverThread = Thread(target=start)
     # serverThread.start()
     # serverThread.join()
+
+    # appのインスタンスを作成
+    app = application
 
     # 毎時00分時点でDBのデータを更新
     schedule.every().hour.at(":00").do(yahoo_finance.generate_all_values)
