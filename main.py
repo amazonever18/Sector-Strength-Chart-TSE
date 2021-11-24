03d6b83dcb812eae6f4db75e7a95f602bc98f4f5
amazonever18 <amazonever18@gmail.com>
2021-11-24 20:54:38 +0900

    'Commit'

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 if __name__ == "__main__":
     # サーバを起動
-    serverThread = Thread(target=start)
-    serverThread.start()
-    serverThread.join()
+    # serverThread = Thread(target=start)
+    # serverThread.start()
+    # serverThread.join()
 
     # 毎時00分時点でDBのデータを更新
     schedule.every().hour.at(":00").do(yahoo_finance.generate_all_values())
